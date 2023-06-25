033aa5b6040db74fb4c1a3f31d7f801f3af5404f
htrm95 <h.rostaminezhad1995@gmail.com>
2023-06-25 11:13:01 +0330

    chenge some to text git

--- HW8/Ripository/IUserRipository.cs
+++ HW8/Ripository/IUserRipository.cs
@@ -6,7 +6,12 @@ namespace HW8.Ripository
     {
         List<User> GetUsers();
         User GetUserByEmail(string email);
+<<<<<<< Updated upstream
         bool Login(string username, string password);
+=======
+        User Login(string username, string password);
+        
+>>>>>>> Stashed changes
         
 
 
