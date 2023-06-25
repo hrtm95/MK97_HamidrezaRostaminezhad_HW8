8d3d6d5763e8028014378f764d5d044dbc49eef2
htrm95 <h.rostaminezhad1995@gmail.com>
2023-06-25 16:23:12 +0330

    add database

--- HW8/Database/Database.cs
+++ HW8/Database/Database.cs
@@ -14,7 +14,11 @@ namespace HW8
         };
         public static List<Turnover> Turnovers = new List<Turnover>()
         {
-            new Turnover(){Id = 1 ,Name ="tejarat" , Credit = 1000 }
+            new Turnover(){Id = 1 ,Name ="tejarat" , Credit = 1000 , Description = "First" , userId = 1 , TransactionTime = DateTime.Parse("2015-05-16") },
+            new Turnover(){Id = 2 ,Name ="tejarat" , Credit = 500 , Description = "secound" , userId = 1 , TransactionTime = DateTime.Parse("2015-05-17") },
+            new Turnover(){Id = 3 ,Name ="melat" , Credit = 2000 , Description = "First" , userId = 2 , TransactionTime = DateTime.Parse("2015-05-18") },
+            new Turnover(){Id = 4 ,Name ="melat" , Credit = 1000 , Description = "secound" , userId = 2 , TransactionTime = DateTime.Parse("2015-05-19") },
+            new Turnover(){Id = 5 ,Name ="tejarat" , Credit = 1000 , Description = "First" , userId = 3 , TransactionTime = DateTime.Parse("2015-05-20") },
         };
     }
 }
