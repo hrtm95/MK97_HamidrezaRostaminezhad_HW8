6f18711e11eb84cce11ce29c3487429774570015
htrm95 <h.rostaminezhad1995@gmail.com>
2023-06-25 10:58:49 +0330

    remove email and add natnalcode and chenge database

--- HW8/Database/Database.cs
+++ HW8/Database/Database.cs
@@ -1,6 +1,16 @@
-﻿namespace HW8.Database
+﻿using HW8.Models;
+
+namespace HW8
 {
-    public class Database
+    public static class Database
     {
+       public static List<User> users = new List<User>()
+        {
+            new User(){Id = 1 ,Name = "Hamid",LastName = "Rostami",PhoneNumber = "1",Natnalcode = "1" },
+            new User(){Id = 2 ,Name = "mamad",LastName = "reza",PhoneNumber = "2",Natnalcode = "2"  },
+            new User(){Id = 3 ,Name = "ahmad",LastName = "hosyeni",PhoneNumber = "3",Natnalcode = "3"  },
+            new User(){Id = 4 ,Name = "jafar",LastName = "gholami",PhoneNumber = "4",Natnalcode = "4" },
+            new User(){Id = 5 ,Name = "akbar",LastName = "bagheri",PhoneNumber = "5",Natnalcode = "5"  }
+        };
     }
 }
--- HW8/Models/User.cs
+++ HW8/Models/User.cs
@@ -5,8 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string LastName { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
+        public string Natnalcode { get; set; }
         public string PhoneNumber { get; set; }
         = string.Empty;
 
