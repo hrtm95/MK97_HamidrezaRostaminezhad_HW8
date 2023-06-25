da561d5ee7059808fd0988ceedf3329cacb7d461
htrm95 <h.rostaminezhad1995@gmail.com>
2023-06-25 16:25:08 +0330

    add turnover to database

--- HW8/Database/Database.cs
+++ HW8/Database/Database.cs
@@ -18,7 +18,7 @@ namespace HW8
             new Turnover(){Id = 2 ,Name ="tejarat" , Credit = 500 , Description = "secound" , userId = 1 , TransactionTime = DateTime.Parse("2015-05-17") },
             new Turnover(){Id = 3 ,Name ="melat" , Credit = 2000 , Description = "First" , userId = 2 , TransactionTime = DateTime.Parse("2015-05-18") },
             new Turnover(){Id = 4 ,Name ="melat" , Credit = 1000 , Description = "secound" , userId = 2 , TransactionTime = DateTime.Parse("2015-05-19") },
-            new Turnover(){Id = 5 ,Name ="tejarat" , Credit = 1000 , Description = "First" , userId = 3 , TransactionTime = DateTime.Parse("2015-05-20") },
+            new Turnover(){Id = 5 ,Name ="tejarat" , Credit = 10000 , Description = "First" , userId = 3 , TransactionTime = DateTime.Parse("2015-05-20") },
         };
     }
 }
