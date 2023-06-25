4a99d07cafad932692b9d91b6bcbcf8b2b5225d2
htrm95 <h.rostaminezhad1995@gmail.com>
2023-06-25 08:10:35 +0330

    add Users model

---
+++ HW8/Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace HW8.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+        = string.Empty;
+
+    }
+}
